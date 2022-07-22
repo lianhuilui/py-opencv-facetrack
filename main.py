56bb4b013a2ee5d052599d036fb1e108ee2a62cc
Lian Hui Lui <lian.hui.lui@gmail.com>
2022-07-22 22:10:21 +0630

    support multiple cams at once

--- main.py
+++ main.py
@@ -4,54 +4,73 @@ from detectors.mp_fm_detector import MPFMDetector
 import tracker
 import cv2 as cv
 
+TOTAL_CAP_DEVICES = 1 # set this to more than 1 to process multiple cams at once
+TRACKER_RES = (220,220)
+
+# Key Bindings
+HOTKEY_TOGGLE_ANNOTATE_TRACKER = 'a'
+HOTKEY_CYCLE_FACE_DETECTION_METHOD = 't'
+
 def main():
-    mp_fm_detector = detectors.MPFMDetector()
-    mp_fd_detector = detectors.MPFDDetector()
-    cv_detector = detectors.CVDetector()
-
     current_detector = 0
-    detector_list = [mp_fm_detector, mp_fd_detector, cv_detector]
 
-    facetracker = tracker.Tracker(mp_fm_detector)
+    detector_list = []
+    trackers_list = []
 
-    cap = cv.VideoCapture(0)
-    cap.set(cv.CAP_PROP_FPS, 30)
+    caps = []
+    for i in range(TOTAL_CAP_DEVICES):
+        # TODO: allow setting camera feed's resolution
+        cap = cv.VideoCapture(i)
 
-    if not cap.isOpened():
-        print("Cannot open camera")
+        if cap.isOpened():
+            caps.append(cap)
+
+            mp_fm_detector = detectors.MPFMDetector()
+            mp_fd_detector = detectors.MPFDDetector()
+            cv_detector = detectors.CVDetector()
+            detector_list.append([mp_fm_detector, mp_fd_detector, cv_detector])
+            trackers_list.append(tracker.Tracker(crop_dim=TRACKER_RES))
+
+    if not caps:
+        print("no capture devices present or available")
         return None
 
     while True:
-        capture_success, cam_img = cap.read()
-        if not capture_success:
-            print("Can't receive frame (stream end?). Exiting ...")
+        cam_imgs = []
+
+        for cap in caps:
+            capture_success, cam_img = cap.read()
+            if capture_success:
+                cam_imgs.append(cam_img)
+
+        if cam_imgs:
+            for idx, cam_img in enumerate(cam_imgs):
+                d = detector_list[idx][current_detector % len(detector_list[idx])]
+                facetracker = trackers_list[idx]
+
+                facetracker.set_detector(d)
+
+                cam_img.flags.writeable=False
+
+                # detect face
+                d.process_frame(cam_img)
+                cv.imshow('CAM %d face detection' % idx, d.output_image)
+
+                # track face
+                facetracker.process_frame(cam_img)
+                cv.imshow('CAM %d tracked' % idx, facetracker.output_image)
+
+                key = cv.waitKey(1)
+
+                if key == ord(HOTKEY_TOGGLE_ANNOTATE_TRACKER):
+                    for t in trackers_list:
+                        t.toggle_annotate()
+
+                if key == ord(HOTKEY_CYCLE_FACE_DETECTION_METHOD):
+                    current_detector += 1
+        else:
+            print ("could not get frame from at least one device")
             break
-
-        cv.imshow('main', cam_img)
-
-        cam_img.flags.writeable=False
-
-        # use mediapipe face mesh
-        mp_fm_detector.process_frame(cam_img)
-        cv.imshow('mp', mp_fm_detector.output_image)
-
-        # use mediapipe face detection
-        mp_fd_detector.process_frame(cam_img)
-        cv.imshow('mp_fd', mp_fd_detector.output_image)
-
-        # use opencv face detector 
-        cv_detector.process_frame(cam_img)
-        cv.imshow('cv', cv_detector.output_image)
-
-        # track face
-        facetracker.process_frame(cam_img)
-        cv.imshow('tracked', facetracker.output_image)
-
-        key = cv.waitKey(1)
-
-        if key == ord('t'):
-            current_detector += 1
-            facetracker.set_detector(detector_list[current_detector % len(detector_list)])
 
     cap.release()
     cv.destroyAllWindows()
--- tracker.py
+++ tracker.py
@@ -2,10 +2,15 @@ from detectors.base import BaseDetector
 
 
 class Tracker():
-    def __init__(self, detector: BaseDetector):
+    def __init__(self, detector: BaseDetector = None, crop_dim = None):
         self.detector = detector
+        self.crop_dim = crop_dim
+        self._annotate = True
         pass
 
+
+    def toggle_annotate(self):
+        self._annotate = not self._annotate
 
     def set_detector(self, value):
         self.detector = value
@@ -15,10 +20,23 @@ class Tracker():
 
         self.output_image = image.copy()
 
-        if self.detector.centerpoint:
-            cv.circle(self.output_image, self.detector.centerpoint,
-                      1, (255, 255, 255), 1, cv.LINE_AA)
+        if self._annotate:
+            if self.detector.centerpoint:
+                cv.circle(self.output_image, self.detector.centerpoint,
+                        1, (255, 255, 255), 1, cv.LINE_AA)
 
-        if self.detector.boundingbox:
-            cv.rectangle(self.output_image, self.detector.boundingbox_start, self.detector.boundingbox_end,
-                         (255, 0, 0), 2, cv.LINE_AA)
+            if self.detector.boundingbox:
+                cv.rectangle(self.output_image, self.detector.boundingbox_start, self.detector.boundingbox_end,
+                            (255, 0, 0), 2, cv.LINE_AA)
+
+        if self.crop_dim and self.detector.centerpoint:
+            w, h = self.crop_dim
+            x = self.detector.centerpoint[0] - (w // 2)
+            y = self.detector.centerpoint[1] - (h // 2)
+
+            x = max (0, x)
+            y = max (0, y)
+            x = min (self.output_image.shape[1] - w, x)
+            y = min (self.output_image.shape[0] - h, y)
+
+            self.output_image = self.output_image[y:y+h, x:x+w]
